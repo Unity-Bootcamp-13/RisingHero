68d5fb0ec93f498f016106f5e71113130a826c47
jisunglim1127 <jisunglim1127@gmail.com>
2025-06-23 18:09:22 +0900

    6/23 (월) 팀 프로젝트 1일?차
    스킬 구조에 대해 고안했음 (큰 의미 없던 뻘짓이었음;)
    
    이 밑으로 오늘 설계한 cs 파일의 용도 작성했음.
    ---------------------------------------------------------------------
    
    ISkill.cs
    - 모든 스킬이 반드시 가져야 할 스킬의 이름, 쿨타임, 발동 및 레벨업 기능을 정의해둔 통합 인터페이스임.
    - 직접 발동이 가능한 액티브 스킬을 정의함.
    - 레벨 업 기능을 갖는 스킬에 대한 인터페이스 (IActivatableSkill)
    - 쿨타임 속성과 쿨타임 갱신 메서드를 정의하는 인터페이스 포함 (IHasCooldown)
    
    SkillManager.cs
    - 현재 활성화 된 스킬들을 관리하고, 자동으로 쿨타임을 갱신하고 스킬을 발동시키는 매니저 클래스로 설계했음.
    - 플레이어가 새로운 스킬 습득 시 호출되어 SkillManager에 등록함.
    - 플레이어가 현재 가지고 있는 모든 스킬을 순회하며, 시간의 흐름(deltaTime)에 맞게 쿨타임을 감소 시키며, 쿨타임이 끝난 액티브 스킬을 발동 시키고 다시 쿨타임을 초기화 시키며 다음 발동까지 대기 시킴.

---
+++ Assets/Scripts/SkillSystem/ISkill.cs
@@ -0,0 +1,34 @@
+
+
+/// <summary>
+/// 모든 스킬이 반드시 가져야 할 이름, 쿨타임, 발동 및 레벨업 기능을 정의한 통합 인터페이스입니다.
+/// </summary>
+public interface ISkill : IActivatableSkill, IUpgradableSkill, IHasCooldown
+{
+    string SkillName { get; }
+}
+
+/// <summary>
+/// 직접 발동이 가능한 액티브 스킬을 정의합니다.
+/// </summary>
+public interface IActivatableSkill
+{
+    void Activate();
+}
+
+/// <summary>
+/// 레벨 업 기능을 갖는 스킬에 대한 인터페이스입니다.
+/// </summary>
+public interface IUpgradableSkill
+{
+    void Upgrade();
+}
+
+/// <summary>
+/// 쿨타임 속성과 쿨타임 갱신 메서드를 정의하는 인터페이스입니다.
+/// </summary>
+public interface IHasCooldown
+{
+    float Cooldown { get; }
+    void UpdateCooldown(float deltaTime);
+}
