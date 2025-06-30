98d96fa69c603fb9fd98b9d18a59cb0981aadb82
정하윤 <gkdnswldbs@gmail.com>
2025-06-30 20:20:34 +0900

    6/30
    다이아몬드 재화 구현(소모, 증가, 소모량 체크)

---
+++ Assets/Scripts/RewardSystem/Diamond/DiamondManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public interface IDiamondManager
+{
+    public void CostDiamond(int cost);
+    public void GetDiamond(int amount);
+    public bool HasEnoughDiamond(int cost);
+}
+
+public class DiamondManager : MonoBehaviour, IDiamondManager
+{
+    [SerializeField] private DiamondUI diamondUI;
+
+    private IDiamondService diamondService;
+
+    public static IDiamondManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        ISaveService saveService = new JsonSaveService();
+        diamondService = new DiamondService(saveService);
+
+        if (Instance != null && !ReferenceEquals(Instance, this))
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        PlayerSaveData data = diamondService.LoadData();
+        diamondUI.UpdateDiamondUI(data.diamond);
+    }
+
+    public void OnButtonClick() // 테스트 용 버튼
+    {
+        GetDiamond(500);
+    }
+
+    public void CostDiamond(int cost)
+    {
+        PlayerSaveData data = diamondService.LoadData();
+        data.diamond -= cost;
+        diamondService.SaveData(data);
+        diamondUI.UpdateDiamondUI(data.diamond);
+    }
+
+    public void GetDiamond(int amount)
+    {
+        PlayerSaveData data = diamondService.LoadData();
+        data.diamond += amount;
+        diamondService.SaveData(data);
+        diamondUI.UpdateDiamondUI(data.diamond);
+    }
+
+    public bool HasEnoughDiamond(int cost)
+    {
+        PlayerSaveData data = diamondService.LoadData();
+        if (data.diamond < cost)
+        {
+            diamondUI.ShowNotEnoughDiamond();
+            return false;
+        }
+
+        return true;
+    }
+}
