27fac1a09e691be79de0da8e04f3c7e34b99d163
SLSL-SL <snwreo95@gmail.com>
2025-07-03 15:50:02 +0900

    QuestSystem 리팩터링을 통한 DDD기반 설계로 수정 작업
    기존의 QuestData, Dialog 등을 삭제하고 Manager의 경우 DDD 기반 설계로 수정.
    DTO 추가, 보상 범위값 구현을 위한 스크립트 추가
    
    이후 서비스 스크립트 구현 예정

---
+++ Assets/Scripts/QuestSystem/QuestManager.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public Quest CurrentQuest => _currentQuest;
+
+    private Quest _currentQuest;
+    private int _questIndex = 0;
+
+    private List<Quest> _questList;
+    private QuestRepository _repository;
+    private QuestRewardService _rewardService;
+
+    private void Awake()
+    {
+        _repository = new QuestRepository();
+        _rewardService = new QuestRewardService();
+
+        _questList = _repository.LoadQuestList();
+
+        LoadCurrentQuest();
+    }
+
+    private void LoadCurrentQuest()
+    {
+        _currentQuest = _repository.LoadQuestProgress();
+
+        if (_currentQuest == null && _questList.Count > 0)
+        {
+            _currentQuest = _questList[_questIndex];
+        }
+    }
+
+    public void AddProgress(QuestType type, int amount)
+    {
+        if (_currentQuest.Type != type || _currentQuest.IsCompleted)
+            return;
+
+        _currentQuest.AddProgress(amount);
+    }
+
+    public bool TryCompleteQuest()
+    {
+        if (!_currentQuest.CanComplete()) return false;
+
+        QuestReward reward = _currentQuest.Complete();
+        _rewardService.Apply(reward);
+
+        _repository.SaveQuestProgress(_currentQuest);
+
+        GoToNextQuest();
+
+        return true;
+    }
+
+    private void GoToNextQuest()
+    {
+        _questIndex++;
+        if (_questIndex >= _questList.Count)
+            _questIndex = 0;
+
+        _currentQuest = _questList[_questIndex];
+        _repository.SaveQuestProgress(_currentQuest);
+    }
+}
---
+++ Assets/Scripts/QuestSystem/QuestReward.cs
@@ -0,0 +1,11 @@
+public class QuestReward
+{
+    public int Gold { get; }
+    public int Jewel { get; }
+
+    public QuestReward(int gold, int jewel)
+    {
+        Gold = gold;
+        Jewel = jewel;
+    }
+}
