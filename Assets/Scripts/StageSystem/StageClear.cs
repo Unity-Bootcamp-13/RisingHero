ef70e83e716f001d9064224ad9a027760fe395e8
정하윤 <gkdnswldbs@gmail.com>
2025-07-06 01:35:44 +0900

    7/06 씬 로더 구현 및 스테이지 버그 수정 #47
    씬 로더를 구현함으로써 해당 스테이지 정상 적용 확인
    스테이지 클리어 시 탑 스테이지가 정상적으로 저장되지 않았던 문제 해결

--- Assets/Scripts/StageSystem/StageClear.cs
+++ Assets/Scripts/StageSystem/StageClear.cs
@@ -2,41 +2,65 @@ using UnityEngine;
 
 public class StageClear : MonoBehaviour
 {
-    [SerializeField] private KillCounter killCounter;
-    [SerializeField] private Timer timer;
+    [SerializeField] private EliteStage eliteStage;
+    [SerializeField] private StageUI stageUI;
 
     private ISaveService saveService;
 
     public void Initialize(ISaveService saveService)
     {
         this.saveService = saveService;
-
-        int stage = saveService.Load().currentStage;
-        if (IsEliteStage(stage))
-        {
-            killCounter?.StartCount(80); // 목표 킬 수
-            killCounter.OnClear += OnStageClear;
-            killCounter.OnFail += OnStageFail;
-
-            timer?.StartTimer(120f); // 제한 시간
-            timer.OnTimeout += () => killCounter?.EndCountByFail();
-        }
     }
 
-    private bool IsEliteStage(int stage)
+    private void Awake()
     {
-        return stage == 19 || stage == 29; // stage % 10 == 9
+        if (eliteStage != null)
+        {
+            eliteStage.OnClear += OnStageClear;
+            eliteStage.OnFail += OnStageFail;
+            return;
+        }
+
+            Debug.LogError("[StageClear] EliteStage가 연결되지 않았습니다.");
+    }
+
+    private void OnDestroy()
+    {
+        if (eliteStage != null)
+        {
+            eliteStage.OnClear -= OnStageClear;
+            eliteStage.OnFail -= OnStageFail;
+        }
     }
 
     private void OnStageClear()
     {
-        timer?.StopTimer();
-        Debug.Log("[StageClear] 스테이지 클리어!");
+        Debug.Log("[StageClear] 엘리트 스테이지 클리어!");
+        // 추가 클리어 처리 작성 가능
+        if (saveService == null)
+        {
+            Debug.LogError("[StageClear] SaveService가 할당되지 않았습니다.");
+            return;
+        }
+
+        var saveData = saveService.Load();
+        saveData.topStage += 1;
+        saveService.Save(saveData);
+
+        //  CoinBuffer에 최신 SaveData를 반영하도록 명시적으로 갱신
+        CoinBuffer.Instance.Initialize(saveService);  // 안전하게 서비스 재주입
+
+        // 클리어 UI 띄우기
+        if (stageUI != null)
+            stageUI.ShowClearWindow();
     }
 
     private void OnStageFail()
     {
-        timer?.StopTimer();
-        Debug.Log("[StageClear] 스테이지 실패!");
+        Debug.Log("[StageClear] 엘리트 스테이지 실패!");
+        // 추가 실패 처리 작성 가능
+        // 클리어 UI 띄우기
+        if (stageUI != null)
+            stageUI.ShowDefeatWindow();
     }
 }
